b378a5bb8dcf8fc6a402df30ccd2a1d64c514a41
primrose101 <purityrosemanlosa@gmail.com>
2021-02-17 18:28:42 +0800

    renamed tokens_type to Tokens & made class Lexer

---
+++ Lexer.py
@@ -0,0 +1,139 @@
+import re
+from Tokens import *
+
+class Lexer:
+    status = 0
+
+    def __init__ (self):
+        status = 0
+
+    def parse(self, statement):
+        statement = self.replaceStmt(statement)
+        tokens = []
+        tokens = statement.split(" ")
+        print(tokens)
+        arrforlex = []
+        actualvalue = []
+        ctr = 0
+
+        for token in tokens:
+            ctr = ctr+1
+            type = 0
+            if token == '':
+                continue
+            else:
+                type = self.checktoken(token)
+                print(token, type)
+            if type == Tokens.ERROR:
+                arrforlex.clear()
+                arrforlex.insert(0, Tokens.ERROR)
+                arrforlex.insert(1, 'Unexpected token: ' + token)
+                break
+            else:
+                arrforlex.insert(ctr, type)
+            actualvalue.insert(ctr, token)
+        return arrforlex, actualvalue
+    
+    def checktoken(self, token):
+        if(re.match(r'VAR [a-zA-Z_$][a-zA-Z_$0-9]*$', token)):
+            type = Tokens.KW_VAR
+        if(re.match(r'^OUTPUT:\s*', token)):
+            type = Tokens.KW_OUTPUT
+        elif (re.match(r'((-*)\d+\.\d+)', token)):
+            type = Tokens.KW_FLOAT
+        elif (re.match(r'((-*)\d)', token)):
+            type = Tokens.KW_INT
+        elif (re.match(r'(^[a-zA-Z_$][a-zA-Z_$0-9]*$)', token)):
+            type = self.parseAlpha(token)
+        elif (re.match(r'.', token)):
+            type = self.parseSpecial(token)
+        else:
+            type = Tokens.ERROR
+        return type
+
+    def parseAlpha(self, token):
+        if token == 'VAR':
+            type = Tokens.KW_VAR
+        elif token == 'AS':
+            type = Tokens.KW_AS
+        elif token == 'INT':
+            type = Tokens.KW_INT
+        elif token == 'CHAR':
+            type = Tokens.CHAR
+        elif token == 'FLOAT':
+            type = Tokens.KW_FLOAT
+        elif token == 'BOOL':
+            type = Tokens.KW_BOOLEAN
+        elif token == 'START':
+            type = Tokens.KW_START
+        elif token == 'STOP':
+            type = Tokens.KW_STOP
+        elif token == 'OUTPUT':
+            type = Tokens.KW_OUTPUT
+        elif token == 'AND':
+            type = Tokens.AND
+        elif token == 'OR':
+            type = Tokens.OR
+        elif token == 'NOT':
+            type = Tokens.NOT
+        elif token == 'TRUE':
+            type = Tokens.BOOL_TRUE
+        elif token == 'FALSE':
+            type = Tokens.BOOL_FALSE
+        return type
+
+    def parseSpecial(self, token):
+        if token == '=':
+            type = Tokens.EQUALS
+        elif token == '(':
+            type = Tokens.PAREN_OPEN
+        elif token == ')':
+            type = Tokens.PAREN_CLOSE
+        elif token == '+':
+            type = Tokens.PLUS
+        elif token == '-':
+            type = Tokens.MINUS
+        elif token == '*':
+            type = Tokens.MULTIPLY
+        elif token == '/':
+            type = Tokens.DIVIDE
+        elif token == '%':
+            type = Tokens.MODULO
+        elif token == '[':
+            type = Tokens.ESCAPE_OPEN
+        elif token == ']':
+            type = Tokens.ESCAPE_CLOSE
+        elif token == '>':
+            type = Tokens.GREATER_THAN
+        elif token == '>=':
+            type = Tokens.GREATER_OR_EQUAL
+        elif token == '<':
+            type = Tokens.LESS_THAN
+        elif token == '<=':
+            type = Tokens.LESS_OR_EQUAL
+        elif token == '<>':
+            type = Tokens.LOGIC_EQUAL
+        elif token == '!=':
+            type = Tokens.NOT_EQUAL
+        else:
+            type = Tokens.ERROR
+        return type        
+        
+    def replaceStmt(self, statement):
+        statement = statement.replace(",", " , ")
+        statement = statement.replace("+", " + ")
+        statement = statement.replace("*", " * ")
+        statement = statement.replace("/", " / ")
+        statement = statement.replace("=", " = ")
+        statement = statement.replace("(", " ( ")
+        statement = statement.replace(")", " ) ")
+        statement = statement.replace("==", "= =")
+        statement = statement.replace("<=", "< =")
+        statement = statement.replace(">=", "> =")
+        statement = statement.replace("<>", " <> ")
+        statement = statement.replace(",", " , ")
+        return statement
+
+
+
+
