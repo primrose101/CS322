a5807c2bf66acdf3044375776c72e958d4fbe1fb
asdpro13 <62065016+asdpro13@users.noreply.github.com>
2021-03-17 00:45:27 +0800

    identifier, operator, logical operator

---
+++ identifier.py
@@ -0,0 +1,43 @@
+"""
+string_input : String -> the text input to lexicalize
+index : int -> where the index is currently at during lexicalization
+"""
+
+
+def iden_fsm(string_input, index):
+    i = index
+
+    table = [
+        (1, 1, 4, 4),
+        (1, 1, 1, 3),
+        (4, 4, 4, 4),
+        (4, 4, 4, 4),
+
+    ]
+    state = 0
+    infut = 0
+    string_length = len(string_input)
+
+    while i < string_length:
+        if string_input[i].isalpha():
+            infut = 0
+        elif string_input[i] == '_':
+            infut = 1
+        elif string_input[i].isdigit():
+            infut = 2
+        else:
+            infut = 3
+
+        state = table[state][infut]
+
+        if state == 4:
+            break
+
+        i += 1
+
+    return i - index
+
+
+#test
+
+print(iden_fsm('STRING123', 0))
---
+++ logicaloperator.py
@@ -0,0 +1,49 @@
+"""
+string_input : String -> the text input to lexicalize
+index : int -> where the index is currently at during lexicalization
+"""
+
+
+def logicalop_fsm(string_input, index):
+    i = index
+
+    table = [
+        (1, 2, 3, 4, 5, 5),
+        (6, 5, 5, 5, 5, 5),
+        (6, 5, 6, 5, 5, 5),
+        (6, 5, 5, 5, 5, 5),
+        (5, 5, 5, 6, 5, 5),
+        (5, 5, 5, 5, 6, 5),
+        (5, 5, 5, 5, 5, 5),
+    ]
+    state = 0
+    infut = 0
+    string_length = len(string_input)
+
+    while i < string_length:
+        if string_input[i] == '=':
+            infut = 0
+        elif string_input[i] == '<':
+            infut = 1
+        elif string_input[i] == '>':
+            infut = 2
+        elif string_input[i] == '&':
+            infut = 3
+        elif string_input[i] == '|':
+            infut = 4
+        else:
+            infut = 5
+
+        state = table[state][infut]
+
+        if state == 5:
+            break
+
+        i += 1
+
+    return i - index
+
+
+#test
+
+print(logicalop_fsm('&a', 0))
