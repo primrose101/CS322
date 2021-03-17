a17d0929e353c03eabd802d454c2de6093728684
_shiro <macbookpro@Shiro.local>
2021-03-18 02:29:34 +0800

    wip

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from DeclarationHandler import DeclarationHandler
 from LogicalExpression import LogicalExpression
 from MathExpression import MathExpression
 from SemanticAnalyzer import SemanticAnalyzer
+from StringExpression import StringExpression
 from Lexer import Lexer
 from Parser import Parser
 import Tokens
@@ -21,7 +22,7 @@ STOP"""
 
 math_expression = 'in = ((3 + 9) / 3 * 6 % 5)'
 boolean_expression = 'he = 100 && FALSE == (TRUE || FALSE ) == 100.0'
-string_expression = "hey = 3 % 24"
+string_expression = 'hey = "NI" & char & "[#]" \'#\' & char'
 
 dec_stmt_no_error = 'VAR hello = \'h\' AS STRING'
 dec_stmt_no_values = 'VAR hey = 3.57, world = 4.0 AS FLOAT'
@@ -32,16 +33,21 @@ lexer = Lexer()
 parser = Parser()
 dec_handler = DeclarationHandler()
 math_evaluator = MathExpression()
+string_evaluator = StringExpression()
 logic_evaluator = LogicalExpression()
 semantics = SemanticAnalyzer()
 variables = {
-    'hey': {'value': 'C', 'type': Tokens.CHAR}
+    'char': {'value': 'C', 'type': Tokens.CHAR}
 }
 
 for line in string_expression.split('\n'):
     token_stream = lexer.lexicalize(line)
     stmt_type = parser.parse_tokens(token_stream)
-    is_valid = semantics.semantic_analyze(token_stream, Tokens.ST_ASSIGNMENT_MATH_FLOAT, variables)
+    is_valid = semantics.semantic_analyze(token_stream, Tokens.ST_ASSIGNMENT_STRICT_STRING, variables)
     print(token_stream)
     print(is_valid)
     print(semantics.status)
+
+    result = string_evaluator.evaluate(token_stream[2:], variables)
+
+    print(result)
