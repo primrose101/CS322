e54601d627deb51ea0b039874eece1f604a23920
primrose101 <purityrosemanlosa@gmail.com>
2021-03-17 23:31:59 +0800

    Keyword Lexer - update

--- KeywordLexer.py
+++ KeywordLexer.py
@@ -215,16 +215,19 @@ def kwfloat_fsm(string_input, index):
 
     return i - index
 
-def kwbool_fsm(string_input, index):
+def kwboolean_fsm(string_input, index):
     i = index
 
     table = [
-        [1,5,5,5,5],
-        [5,2,5,5,5],
-        [5,3,5,5,5],
-        [5,5,5,4,5],
-        [5,5,5,5,5], 
-        [5,5,5,5,5],         
+        [1,8,8,8,8,8,8,8],
+        [8,2,8,8,8,8,8,8],
+        [8,3,8,8,8,8,8,8],
+        [8,8,8,4,8,8,8,8],
+        [8,8,8,8,5,8,8,8], 
+        [8,8,8,8,8,6,8,8],  
+        [8,8,8,8,8,8,7,8],
+        [8,8,8,8,8,8,8,8],    
+        [8,8,8,8,8,8,8,8],   
     ]
 
     state = 0
@@ -241,28 +244,77 @@ def kwbool_fsm(string_input, index):
             inputstate = 2
         elif string_input[i] == 'L':
             inputstate = 3
-        else:
+        elif string_input[i] == 'E':
             inputstate = 4
+        elif string_input[i] == 'A':
+            inputstate = 5
+        elif string_input[i] == 'N':
+            inputstate = 6
+        else:
+            inputstate = 7
 
         state = table[state][inputstate]
 
-        if state == 5:
+        if state == 8:
+            break
+
+        i += 1
+
+    return i - index
+    
+def kwinput_fsm(string_input, index):
+    i = index 
+
+    table = [
+        [1,6,6,6,6,6],
+        [6,2,6,6,6,6],
+        [6,6,3,6,6,6],
+        [6,6,6,4,6,6],
+        [6,6,6,6,5,6],
+        [6,6,6,6,6,6],
+        [6,6,6,6,6,6],
+    ]
+
+    state = 0
+    inputstate = 0
+
+    string_length = len(string_input)
+
+    while i != string_length:
+        if string_input[i] == 'I':
+            inputstate = 0
+        elif string_input[i] == 'N':
+            inputstate = 1
+        elif string_input[i] == 'P':
+            inputstate = 2
+        elif string_input[i] == 'U':
+            inputstate = 3
+        elif string_input[i] == 'T':
+            inputstate = 4
+        else:
+            inputstate = 5
+
+        state = table[state][inputstate]
+
+        if state == 6:
             break
 
         i += 1
 
     return i - index
 
-
-def kwout_fsm(string_input, index):
+def kwoutput_fsm(string_input, index):
     i = index
 
     table = [
-        [1,4,4,4],
-        [4,2,4,4],
-        [4,4,3,4],
-        [4,4,4,4],
-        [4,4,4,4],
+        [1,7,7,7,7,7,7],
+        [7,2,7,7,7,7,7],
+        [7,7,3,7,7,7,7],
+        [7,7,7,4,7,7,7],
+        [7,5,7,7,7,7,7],
+        [7,7,6,7,7,7,7],
+        [7,7,7,7,7,7,7],
+        [7,7,7,7,7,7,7],
     ]
 
     state = 0
@@ -277,12 +329,56 @@ def kwout_fsm(string_input, index):
             inputstate = 1
         elif string_input[i] == 'T':
             inputstate = 2
-        else:
+        elif string_input[i] == 'P':
             inputstate = 3
+        elif string_input[i] == 'U':
+            inputstate = 4
+        elif string_input[i] == 'T':
+            inputstate = 5
+        else:
+            inputstate = 6
 
         state = table[state][inputstate]
 
-        if state == 4:
+        if state == 7:
+            break
+
+        i += 1
+
+    return i - index
+
+def kwchar_fsm(string_input, index):
+    i = index
+
+    table = [
+        [1,5,5,5,5],
+        [5,2,5,5,5],
+        [5,5,3,5,5],
+        [5,5,5,4,5],
+        [5,5,5,5,5], 
+        [5,5,5,5,5],         
+    ]
+
+    state = 0
+    inputstate = 0
+
+    string_length = len(string_input)
+
+    while i != string_length:
+        if string_input[i] == 'C':
+            inputstate = 0
+        elif string_input[i] == 'H':
+            inputstate = 1
+        elif string_input[i] == 'A':
+            inputstate = 2
+        elif string_input[i] == 'R':
+            inputstate = 3
+        else:
+            inputstate = 4
+
+        state = table[state][inputstate]
+
+        if state == 5:
             break
 
         i += 1
@@ -350,6 +446,36 @@ def kwas_fsm(string_input, index):
         state = table[state][inputstate]
 
         if state == 3:
+            break
+
+        i += 1
+
+    return i - index
+
+def kwcolon_fsm(string_input, index):
+    i = index
+
+    table = [
+        [1,2],
+        [2,2],
+        [2,2],
+        
+    ]
+
+    state = 0
+    inputstate = 0
+
+    string_length = len(string_input)
+
+    while i != string_length:
+        if string_input[i] == ':':
+            inputstate = 0
+        else:
+            inputstate = 1
+
+        state = table[state][inputstate]
+
+        if state == 2:
             break
 
         i += 1
