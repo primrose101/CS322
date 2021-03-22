09d95c7a26954521cd435d9ad01e410f76d3dbe1
_shiro <macbookpro@Shiro.local>
2021-03-22 17:55:33 +0800

    fixed string token error

--- test.py
+++ test.py
@@ -2,13 +2,8 @@ from Interpreter import interpret
 
 # test your statements here
 statement = """
-VAR num AS INT
-VAR bool AS BOOLEAN
 START
-num = 100 + (((66 - 30))) * (-2 / 1) 
-bool = TRUE && (((FALSE && TRUE || FALSE)))
-OUTPUT: "Hello, World!" & "[#"
-OUTPUT: bool
+OUTPUT: "Hi" & ""
 STOP
 """
 
